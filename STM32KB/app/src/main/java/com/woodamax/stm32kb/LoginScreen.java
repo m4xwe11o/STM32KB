6c77a3e3a4871df0db009f2e4bbf82233694507d
m4xwe11o <maximilian.pessl@gmail.com>
2017-03-24 11:48:18 +0100

    Added menu functions

--- STM32KB/app/src/main/java/com/woodamax/stm32kb/LoginScreen.java
+++ STM32KB/app/src/main/java/com/woodamax/stm32kb/LoginScreen.java
@@ -1,10 +1,13 @@
 package com.woodamax.stm32kb;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -18,8 +21,9 @@ public class LoginScreen extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_login_toolbar);
         // Sets the Toolbar to act as the ActionBar for this Activity window.
         // Make sure the toolbar exists in the activity and is not null
+        toolbar.setBackgroundColor(Color.parseColor("#F44336"));
         setSupportActionBar(toolbar);
-
+        
         // add back arrow to toolbar
         if (getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -40,6 +44,8 @@ public class LoginScreen extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        //To change the color in the action bar we need to define the toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_login_toolbar);
         // handle arrow click here
         if (item.getItemId() == android.R.id.home) {
             finish(); // close this activity and return to preview activity (if there is any)
@@ -48,6 +54,23 @@ public class LoginScreen extends AppCompatActivity {
             Toast.makeText(LoginScreen.this,"Clicked on info",Toast.LENGTH_SHORT).show();
         }
 
-        return super.onOptionsItemSelected(item);
+        //Just something for customazation
+        int id = item.getItemId();
+        switch (id){
+            case R.id.menu_blue:
+                if(item.isChecked()){item.setChecked(false);}else{item.setChecked(true);}
+                toolbar.setBackgroundColor(Color.parseColor("#2196F3"));
+                return true;
+            case R.id.menu_red:
+                if(item.isChecked()){item.setChecked(false);}else{item.setChecked(true);}
+                toolbar.setBackgroundColor(Color.parseColor("#F44336"));
+                return true;
+            case R.id.menu_green:
+                if(item.isChecked()){item.setChecked(false);}else{item.setChecked(true);}
+                toolbar.setBackgroundColor(Color.parseColor("#4CAF50"));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
